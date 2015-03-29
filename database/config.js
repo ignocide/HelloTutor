4bde454ecfb7121d7d7495e4d877062dde402715
ignocide <ignocide@gmail.com>
2015-03-30 03:03:10 +0900

    git up project

--- database/config.js
+++ database/config.js
@@ -3,16 +3,16 @@ var nodemailer = require('nodemailer');
 exports.pool  = mysql.createPool( {
     connectionLimit : 150,
     host     : '54.65.219.91',
-    user     : 'hello_test',
-    password : '1234',
-    database : 'hello_tutor'
+    user     : 'secure',
+    password : 'secure',
+    database : 'secure'
 //    database : 'hello_test'
 });
 
 exports. transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'cjh05044@gmail.com',
-        pass: 'junho714'
+        user: 'secure',
+        pass: 'secure'
     }
 });
